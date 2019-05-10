b3571fe1cb87e5d7ea94624ad55a9a4b6cc85ea7
gerry <g.petricca@rheinmetall.it>
2019-05-10 11:31:18 +0200

    Gestione della dialog per la cancellazione della riga del database

--- app/src/main/java/com/example/world_bank_v4/DialogDeleteRow.java
+++ app/src/main/java/com/example/world_bank_v4/DialogDeleteRow.java
@@ -4,27 +4,33 @@ import android.app.AlertDialog;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.util.Log;
+import android.view.View;
 
 
-public class DialogDeleteRow extends AppCompatDialogFragment {
+public class DialogDeleteRow extends AppCompatDialogFragment{
 
-    private String scelta_utente;
+    public interface OnClickListener {
+        void onFinishClickListener(String inputText);
+    }
 
-
-    public Dialog onCreateDialog(final Bundle savedInstanceState){
+    public Dialog onCreateDialog(Bundle savedInstanceState){
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         Log.d(Costanti.NOME_APP, "builder ok");
         builder.setTitle("Warning!")
                 .setIcon(R.drawable.warning)
                 .setMessage("Do you really want to delete it?")
                 .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        /*CaricaDati.deleterow();*/
-                        scelta_utente = "YES";
+                        OnClickListener listener = (OnClickListener) getActivity();
+                        listener.onFinishClickListener("delete");
+                        DialogDeleteRow.this.dismiss();
                     }
                 });
 
@@ -35,13 +41,8 @@ public class DialogDeleteRow extends AppCompatDialogFragment {
             }
         });
 
-        return builder.show();
+        return builder.create();
 
-    }
-
-
-    public String getScelta_utente(){
-        return scelta_utente;
     }
 
 }
