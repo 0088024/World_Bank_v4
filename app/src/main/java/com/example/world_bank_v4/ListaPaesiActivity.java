3c3f29139f25f0a4a5d5df3a2fb11366faa0f2ff
daniele <f.daniele81@tiscali.it>
2019-05-10 18:11:54 +0200

    miglioramenti ereditarietà+++

--- app/src/main/java/com/example/world_bank_v4/ListaPaesiActivity.java
+++ app/src/main/java/com/example/world_bank_v4/ListaPaesiActivity.java
@@ -30,8 +30,7 @@ public class ListaPaesiActivity extends ListaGenericaActivity {
 
         setContentView(R.layout.activity_lista_paese_activity);
         getSupportActionBar().setLogo(R.drawable.country);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         progressBar1 = findViewById(R.id.progressBar1);
         ArrayList<Paese> lista_paesi = new ArrayList<Paese>();
         TypeToken<ArrayList<Paese>> listTypeToken = new TypeToken<ArrayList<Paese>>() {};
